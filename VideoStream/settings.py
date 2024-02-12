a485ed8f7833ae5d06f2ba1cfe43c37ba4310f01
AftabUddin9 <aftabrafi9@gmail.com>
2024-02-12 15:46:26 +0600

    ready to use

--- VideoStream/settings.py
+++ VideoStream/settings.py
@@ -15,8 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = BASE_DIR.joinpath('templates')
-STATIC_DIR = BASE_DIR.joinpath('static')
-MEDIA_DIR = BASE_DIR.joinpath('media')
+# STATIC_DIR = BASE_DIR.joinpath('static')
+# MEDIA_DIR = BASE_DIR.joinpath('media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,11 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [STATIC_DIR,]
+# STATICFILES_DIR = [STATIC_DIR,]
 
 # Media files
-MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = '/media/'
+# MEDIA_ROOT = MEDIA_DIR
+# MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
